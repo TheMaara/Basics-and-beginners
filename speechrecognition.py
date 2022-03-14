43df13e0b285889861cc9aaf0b2b132cd38542c1
TheMaara <68475422+TheMaara@users.noreply.github.com>
2022-03-14 04:24:05 +0300

    Update speechrecognition.py

--- speechrecognition.py
+++ speechrecognition.py
@@ -1,7 +1,20 @@
 import speech_recognition
 import pyttsx3
 
+def speak(text):
+    engine = pyttsx3.init()
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', 150)
 
+    #Setting the voice
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+
+    #Text input
+    engine.say(text)
+    engine.runAndWait()
+
+speak("Say something...")
 recognizer = speech_recognition.Recognizer()
 
 while True:
@@ -14,9 +27,9 @@ while True:
             audio = recognizer.listen(mic)
 
             text = recognizer.recognize_google(audio)
-            text = lower()
+            text = text.lower()
 
-            print(f"Recognized {text}")
+            print(f"Recognized: {text}")
 
     except speech_recognition.UnknownValueError():
 
